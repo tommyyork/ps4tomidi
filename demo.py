274ec3555f40c6029d0f0d155fbc53df71d06c16
tommy york <tommy.york@gmail.com>
2019-04-21 12:40:44 -0400

    No graceful shutdown, but using asyncio event loop.

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import pygame
 
 from classes import BeatStepPro
@@ -6,10 +8,11 @@ from classes import PS4Controller
 ps4 = PS4Controller.PS4Controller()
 bsp = BeatStepPro.BeatStepPro()
 
-ps4.init()
 
-while True:
-    for event in pygame.event.get():
+def handle_ps4_events():
+    while True:
+        event = pygame.event.wait()
+
         if event.type == pygame.JOYAXISMOTION:
             ps4.axis_data[event.axis] = round(event.value, 2)
         elif event.type == pygame.JOYBUTTONDOWN:
@@ -19,14 +22,28 @@ while True:
         elif event.type == pygame.JOYHATMOTION:
             ps4.hat_data[event.hat] = event.value
 
-        # if ps4.hat_data[0][0] == 1:
-        #     print('Attempting to play C.')
-        #     bsp.play_c()
+            # if ps4.hat_data[0][0] == 1:
+            #     print('Attempting to play C.')
+            #     bsp.play_c()
 
         x1 = max(round(64 + (ps4.axis_data[0] * 63.5)), 1) - 1
         y1 = max(round(64 + (-ps4.axis_data[1] * 63.5)), 1) - 1
         x2 = max(round(64 + (ps4.axis_data[2] * 63.5)), 1) - 1
         y2 = max(round(64 + (-ps4.axis_data[3] * 63.5)), 1) - 1
 
+        print('attempting to update output')
+
         bsp.updateAxes({0: x1, 1: y1, 2: x2, 3: y2})
         bsp.updateOutput()
+
+        asyncio.get_event_loop().stop()
+
+
+async def main():
+    ps4.init()
+
+    asyncio.create_task(handle_ps4_events())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
